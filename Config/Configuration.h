6e18783f0ccca14a7da276cf138e38f17bc92127
Stuart Brown <stuartbrown@duck.com>
2024-04-13 20:36:06 +1200

    Show my custom version on main screen

--- Config/Configuration.h
+++ Config/Configuration.h
@@ -141,7 +141,7 @@
 
 // Name displayed in the LCD "Ready" message and Info menu
 // ALTERED: 
-#define CUSTOM_MACHINE_NAME "Ender-3 (4.2.2)"
+#define CUSTOM_MACHINE_NAME "Ender-3+ (240413)"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
