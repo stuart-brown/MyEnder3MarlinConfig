7379348207424470ab2eca9189d89524a1309633
Stuart Brown <stuartbrown@duck.com>
2024-04-13 19:02:07 +1200

    Setup levelling mesh for blue clamps front and back

--- Config/Configuration_adv.h
+++ Config/Configuration_adv.h
@@ -2179,10 +2179,11 @@
 
 #if EITHER(MESH_BED_LEVELING, AUTO_BED_LEVELING_UBL)
   // Override the mesh area if the automatic (max) area is too large
-  //#define MESH_MIN_X MESH_INSET
-  //#define MESH_MIN_Y MESH_INSET
-  //#define MESH_MAX_X X_BED_SIZE - (MESH_INSET)
-  //#define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET)
+  // Setup mesh for blue clamps at front and back, nothing to sides
+  #define MESH_MIN_X 0
+  #define MESH_MIN_Y MESH_INSET
+  #define MESH_MAX_X X_BED_SIZE
+  #define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET)
 #endif
 
 #if BOTH(AUTO_BED_LEVELING_UBL, EEPROM_SETTINGS)
